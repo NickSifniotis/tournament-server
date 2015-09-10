e15dbf99959c5d6da744ba67139b9efd941f7192
nsifniotis <u5809912@anu.edu.au>
2015-09-10 20:04:37 +1000

    Bizarre experimentation with fixturing

---
+++ src/TournamentServer/Fixtures/Fixture.java
@@ -0,0 +1,103 @@
+package TournamentServer.Fixtures;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by nsifniotis on 10/09/15.
+ *
+ */
+public class Fixture
+{
+    private List<short[]> rounds;
+    private short[][] met_before;
+    private static final int NUM_PLAYERS = 10;
+
+    public Fixture()
+    {
+        rounds = new LinkedList<>();
+        met_before = new short[NUM_PLAYERS][NUM_PLAYERS];
+    }
+
+    public boolean perfect()
+    {
+        short[] counts = new short[10];
+
+        for (int i = 0; i < NUM_PLAYERS; i++)
+            for (int j = 0; j < NUM_PLAYERS; j++)
+                counts[met_before[i][j]]++;
+
+        counts[0] -= NUM_PLAYERS;       // coz they dont play themselves
+
+        int zero_count = 0;
+        for (short c: counts)
+            zero_count += (c == 0) ? 1 : 0;
+
+        return (zero_count == NUM_PLAYERS - 1);
+    }
+
+    public int range ()
+    {
+        int min = 15;
+        int max = -1;
+
+        for (int i = 0; i < NUM_PLAYERS; i++)
+            for (int j = 0; j < NUM_PLAYERS; j++)
+            {
+                if (min > met_before[i][j])
+                    min = met_before[i][j];
+
+                if (max < met_before[i][j])
+                    max = met_before[i][j];
+            }
+
+        return max - min;
+    }
+
+
+
+    public String hash()
+    {
+        short[] counts = new short[10];
+
+        for (int i = 0; i < NUM_PLAYERS; i++)
+            for (int j = 0; j < NUM_PLAYERS; j++)
+                counts[met_before[i][j]]++;
+
+        counts[0] -= NUM_PLAYERS;       // coz they dont play themselves
+
+        String res = "";
+        for (short s : counts)
+            res += s + ":";
+
+        return res;
+    }
+
+
+    public Fixture AddRound(short[] round)
+    {
+        // need to create a deep copy of this object.
+
+        Fixture res = new Fixture();
+        for (int i = 0; i < NUM_PLAYERS; i++)
+            for (int j = 0; j < NUM_PLAYERS; j++)
+                res.met_before[i][j] = this.met_before[i][j];
+
+        for (short[] r : this.rounds)
+            res.rounds.add(r.clone());
+
+        res.rounds.add(round);
+
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                if (i != j)
+                    res.met_before[round[i]][round[j]]++;
+
+        for (int i = 4; i < 8; i++)
+            for (int j = 4; j < 8; j++)
+                if (i != j)
+                    res.met_before[round[i]][round[j]]++;
+
+        return res;
+    }
+}
--- src/TournamentServer/tester.java
+++ src/TournamentServer/tester.java
@@ -4,6 +4,11 @@ import AcademicsInterface.IGameEngine;
 import Common.DataModel.Game;
 import Common.DataModel.PlayerSubmission;
 import Common.DataModel.Tournament;
+import TournamentServer.Fixtures.Fixture;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by nsifniotis on 7/09/15.
@@ -13,30 +18,196 @@ import Common.DataModel.Tournament;
  */
 public class tester {
 
+    private static final short NUM_PLAYERS = 10;
+
+
+    /**
+     * Sorts the game array so that the lowest players come first. This
+     * makes comparing two rounds easy since two identical rounds will
+     * sort the same way.
+     * @param original the unsorted round
+     */
+    public static short [] sort_round(short[] original)
+    {
+        short [] res = new short[8];
+
+        short [] game1 = new short[4];
+        short [] game2 = new short[4];
+
+        System.arraycopy(original, 0, game1, 0, 4);
+        System.arraycopy(original, 4, game2, 0, 4);
+
+        game1 = sort_array(game1);
+        game2 = sort_array(game2);
+
+        if (game1[0] < game2[0])
+        {
+            System.arraycopy(game1, 0, res, 0, 4);
+            System.arraycopy(game2, 0, res, 4, 4);
+        }
+        else
+        {
+            System.arraycopy(game2, 0, res, 0, 4);
+            System.arraycopy(game1, 0, res, 4, 4);
+        }
+
+        return res;
+    }
+
+
+    public static short [] sort_array (short [] array)
+    {
+        for (int i = 0; i < array.length; i ++)
+            for (int j = 0; j < array.length - 1; j ++)
+                if (array[j] > array[j + 1])
+                {
+                    short temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+
+        return array;
+    }
+
+
+    public static String hash_round (short [] original)
+    {
+        String res = "";
+
+        for (short s: original)
+            res += s;
+
+        return res;
+    }
+
     public static void main(String[] args)
     {
+        // this is an interesting one. Experimenting with fixturing.
 
-        // get the tournament data
-        Tournament [] tourneys = Tournament.LoadAll();
+        // the hypothetical is a ten player tournament with four players per game
+        // this means that there will be two games and two byes per round.
 
-        if (tourneys.length == 0)
-            return;
+        // the endgame is to create an algorithm that will generate fixtures for tournaments
+        // with an arbitary number of competitors, with an arbitary number of players per game
 
-        // create a game engine
-        IGameEngine engine = tourneys[0].GameEngine();
+        // one round is two 4player games. There is no need to represent the teams in the bye
+        // in this data structure. Therefore, an 8 element ushort array should do the job.
 
-        if (engine == null)
-            return;
+        short [] round;
+        short [] players = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int num_games = 0;
+
+        HashMap <String, short []> unique_games = new HashMap<>();
+
+        boolean finished = false;
+        while (!finished)
+        {
+            // Find the largest index k such that a[k] < a[k + 1].
+            // If no such index exists, the permutation is the last permutation.
+
+            int k = -1;
+            for (int i = 0; i < players.length - 1; i ++)
+                if (players[i] < players[i + 1])
+                    k = i;
+
+            if (k == -1)
+                finished = true;
+
+            // the rest of the algorithm goes here.
+            if (!finished)
+            {
+                // Find the largest index l greater than k such that a[k] < a[l].
+                int l = -1;
+                for (int i = k; i < players.length; i ++)
+                    if (players[k] < players[i])
+                        l = i;
+
+                //Swap the value of a[k] with that of a[l].
+                short temp = players[k];
+                players[k] = players[l];
+                players[l] = temp;
+
+                // Reverse the sequence from a[k + 1] up to and including the final element a[n].
+                short [] newarray = players.clone();
+
+                for (int i = k + 1; i < players.length; i ++)
+                    newarray[players.length - i + k] = players[i];
+
+                players = newarray;
 
 
-        // create some players
-        PlayerSubmission player = new PlayerSubmission(15);
-        PlayerManager [] game_players = new PlayerManager[4];
-        for (int i = 0; i < 4; i ++)
-            game_players[i] = new PlayerManager(tourneys[0], player);
+                round = new short[8];
+                System.arraycopy(players, 0, round, 0, round.length);
+                round = sort_round(round);
 
-        GameManagerChild child = new GameManagerChild(new Game(1), engine, game_players);
+                if (unique_games.putIfAbsent(hash_round(round), round) == null)
+                {
+                    System.out.println(hash_round(round));
+                    num_games++;
+                }
+            }
+        }
 
-        child.run();
+
+        System.out.println ("Finished! Total unique combinations: " + num_games);
+
+        Fixture start = new Fixture();
+
+        HashMap<String, Fixture> fixtures = new HashMap<>();
+        HashMap<String, Fixture> new_fixtures;
+        Fixture temp;
+        int count;
+        int best_range;
+
+        temp = start.AddRound(unique_games.get("01234567"));
+        fixtures.putIfAbsent(temp.hash(), temp);
+
+        for (int zzz = 0; zzz < 10; zzz ++)
+        {
+            count = 0;
+            best_range = 20; // impossible
+            new_fixtures = new HashMap<>();
+
+            for (short[] r : unique_games.values())
+                for (Fixture f : fixtures.values())
+                {
+                    temp = f.AddRound(r);
+
+                    if (temp.perfect())
+                    {
+                        System.out.println ("Perfect fixture found!!");
+                        System.out.println (temp.hash());
+                        return;
+
+                    }
+
+                    if (temp.range() <= best_range)
+                    {
+                        // filters out a lot of fat before it's even inserted into the hashmap
+                        if (new_fixtures.putIfAbsent(temp.hash(), temp) == null)
+                        {
+                            System.out.println("Adding hash " + temp.hash() + " range " + temp.range());
+                            count++;
+                            best_range = temp.range();
+                        }
+                    }
+                }
+
+            fixtures = new_fixtures;
+
+            System.out.println("Total adds: " + count);
+            count = 0;
+
+            // filter out fixtures that have blown out their ranges too far.
+            new_fixtures = new HashMap<>();
+            for (Fixture f: fixtures.values())
+                if (f.range() == best_range)
+                    new_fixtures.putIfAbsent(f.hash(), f);
+                else
+                    count ++;
+
+            System.out.println ("Total fails: " + count);
+            fixtures = new_fixtures;
+        }
     }
 }
