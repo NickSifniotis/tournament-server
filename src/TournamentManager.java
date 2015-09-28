05c0bd9faa6b6d5a40ad5173aea36bec496295ab
nsifniotis <u5809912@anu.edu.au>
2015-09-28 21:49:29 +1000

    Started constructing TournamentManager GUI and connecting other windows to it.

--- src/TournamentManager.java
+++ src/TournamentManager.java
@@ -1,5 +1,5 @@
 /**
- * Created by nsifniotis on 20/09/15.
+ * Created by nsifniotis on 28/09/15.
  *
  * The master controller class
  *
@@ -9,6 +9,66 @@
  * Runs the services - resources, emails, logs, databases, twitter feed etc etc
  *
  */
-public class TournamentManager
+
+import GameManager.GameManager;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class TournamentManager extends Application
 {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    /**
+     * Nick Sifniotis u5809912
+     * 28/09/2015
+     *
+     * Build the main controller GUI.
+     *
+     * Right now, it's just going to be a bunch of buttons.
+     *
+     * @param primaryStage - THE STAGE IS SET FOR THIS MIGHTIEST OF TOURNAMENTS
+     */
+    @Override
+    public void start(Stage primaryStage)
+    {
+        Button game_manager_btn = new Button("Open Game Manager");
+        game_manager_btn.setOnAction(e -> launch_game_manager());
+
+        HBox main_row = new HBox();
+        main_row.setSpacing(10);
+        main_row.getChildren().add(game_manager_btn);
+
+        Scene scene = new Scene(main_row);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tournament Server 2015");
+        primaryStage.show();
+    }
+
+
+    /**
+     * Nick Sifniotis u5809912
+     * 28/09/2015
+     *
+     * Button onClick event handlers.
+     */
+    private void launch_game_manager ()
+    {
+        GameManager new_window = new GameManager();
+        try
+        {
+            new_window.start(new Stage());
+        }
+        catch (Exception e)
+        {
+            // fuck off
+        }
+    }
 }
